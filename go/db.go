2e3c750a664b30c645ed6eda478e349935038b64
Kip <maria.robertslemus@gmail.com>
2023-11-16 17:19:41 -0600

    fixing register.html
    may just rollback and post to separate branch

--- go/db.go
+++ go/db.go
@@ -1,77 +1,79 @@
 package main
 
 import (
-    "log"
-    "os"
-    "database/sql"
-    "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"log"
+	"os"
 
+	"github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func init() {
-    // Capture connection properties.
-    cfg := mysql.Config {
-        User:   os.Getenv("DBUSER"),
-        Passwd: os.Getenv("DBPASS"),
-        Net:    "tcp",
-        Addr:   "back2books.xyz:3306",
-        DBName: "back2books",
-    }
-    // Get a database handle.
-    var err error
-    db, err = sql.Open("mysql", cfg.FormatDSN())
-    if err != nil {
-        log.Fatal(err)
-    }
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "back2books.xyz:3306",
+		DBName: "back2books",
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // Inserts a book into the BOOKS table.
 func createBook(Title string, Lname string, Fname string, Price float32, Publisher string) error {
-    _, err := db.Exec(
-        "INSERT INTO BOOKS (title, author_lname, author_fname, price, publisher) values (?, ?, ?, ?, ?)",
-    Title, Lname, Fname, Price, Publisher)
+	_, err := db.Exec(
+		"INSERT INTO BOOKS (title, author_lname, author_fname, price, publisher) values (?, ?, ?, ?, ?)",
+		Title, Lname, Fname, Price, Publisher)
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    return nil
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
 
 // Deletes a book from the BOOKS table given an id.
 func deleteBook(id int) error {
-    _, err := db.Exec("DELETE FROM BOOKS WHERE id=?", id)
+	_, err := db.Exec("DELETE FROM BOOKS WHERE id=?", id)
 
-    if err != nil {
-        log.Fatal(err)
-    }
-    return nil
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
 
 // Returns all items in the BOOKS table.
 func getBooks() ([]Book, error) {
-    rows, err := db.Query("SELECT * FROM BOOKS")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
+	rows, err := db.Query("SELECT * FROM BOOKS")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 
-    var books []Book
+	var books []Book
 
-    for rows.Next() {
-        var book Book
-        err := rows.Scan(&book.ID, &book.Title, &book.Desc, &book.ISBN, &book.PublishDate, &book.Price,&book.Genre, &book.Format, &book.NumPages, &book.Publisher)
-        if err != nil {
-            log.Fatal(err)
-        }
-        books = append(books, book)
-    }
-    if err := rows.Err(); err != nil {
-        log.Fatal(err)
-    }
+	for rows.Next() {
+		var book Book
+		err := rows.Scan(&book.ID, &book.Title, &book.Desc, &book.ISBN, &book.PublishDate, &book.Price, &book.Genre, &book.Format, &book.NumPages, &book.Publisher)
+		if err != nil {
+			log.Fatal(err)
+		}
+		books = append(books, book)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    return books, nil
+	return books, nil
 }
--- go/main.go
+++ go/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-var db *sql.DB
+//var db *sql.DB
 
 func main() {
-    // http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	// http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 
-    fmt.Println("running main")
-    http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/login", loginHandler)
-    http.HandleFunc("/register", registerHandler)
-    http.HandleFunc("/pages/products.html", booksHandler)
-    http.HandleFunc("/fetch", fetchHandler)
-    http.HandleFunc("/add", AddHandler)
-    http.HandleFunc("/delete", DeleteHandler)
+	fmt.Println("running main")
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/pages/products.html", booksHandler)
+	http.HandleFunc("/fetch", fetchHandler)
+	http.HandleFunc("/add", AddHandler)
+	http.HandleFunc("/delete", DeleteHandler)
 
-    fmt.Println("Server started on :8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started on :8080")
+
+	//getBooks()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
