b774c9100bf238be33ea161e8291d8221394fd81
nielmin <whadmia@gmail.com>
2023-11-18 14:06:58 -0600

    Update file paths and css

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 
 	fmt.Println("running main")
